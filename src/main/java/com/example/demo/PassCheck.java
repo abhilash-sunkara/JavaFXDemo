6d8e48f604cd06bcaf6907b5711259b18c0ea9fc
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2022-08-19 08:53:59 -0500

    input and checking input

--- src/main/java/com/example/demo/PassCheck.java
+++ src/main/java/com/example/demo/PassCheck.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -13,6 +14,7 @@ public class PassCheck extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Password Checker");
+        String pass = "password";
         Group IFX = new Group();
         Scene intro = new Scene(IFX, 320, 400);
         Text t = new Text("Your Password is \"password\"");
@@ -21,6 +23,26 @@ public class PassCheck extends Application {
 
         Group PFX = new Group();
         Scene passScreen = new Scene(PFX, 320, 400);
+
+        Text tCheck = new Text("no password entered");
+        tCheck.setY(100);
+
+        TextField tf = new TextField("enter your password");
+        EventHandler<ActionEvent> checkEV = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println(tf.getText());
+                System.out.println(tf.getText().equals(pass));
+                if (tf.getText().equals(pass)){
+                    tCheck.setText("correct ");
+                }
+                else{
+                    tCheck.setText("wrong");
+                }
+            }
+        };
+        tf.setOnAction(checkEV);
+
 
 
         Button nextB = new Button("click me to go the next page");
@@ -34,8 +56,12 @@ public class PassCheck extends Application {
         };
         nextB.setOnAction(nextEV);
 
+
+
         IFX.getChildren().add(t);
         IFX.getChildren().add(nextB);
+        PFX.getChildren().add(tf);
+        PFX.getChildren().add(tCheck);
         stage.setScene(intro);
         stage.show();
     }
