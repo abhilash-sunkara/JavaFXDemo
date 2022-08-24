c2862477e5e2cd0eaae445cc8920312e6b22daff
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2022-08-24 09:52:36 -0500

    moving image

--- src/main/java/com/example/demo/LoadImage.java
+++ src/main/java/com/example/demo/LoadImage.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
@@ -20,9 +25,29 @@ public class LoadImage extends Application {
         Image image = new Image(inputstream);
         ImageView imageView = new ImageView(image);
         imageView.setPreserveRatio(true);
+        imageView.setFitHeight(200);
         imageView.setX(200);
         imageView.setY(200);
-        IMFX.getChildren().add(imageView);
+
+        int brClick = 0;
+        Text imgT = new Text("Breaches Clicked");
+        imgT.setX(100);
+        imgT.setFill(Color.BLACK);
+
+        Button imgB = new Button();
+        imgB.setGraphic(imageView);
+        EventHandler<ActionEvent> imgEV = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Clicked");
+                imgB.setTranslateX((Math.random()*600));
+                imgB.setTranslateY((Math.random()*800));
+            }
+        };
+        imgB.setOnAction(imgEV);
+
+        IMFX.getChildren().add(imgB);
+        IMFX.getChildren().add(imgT);
         stage.setScene(IS);
         stage.show();
     }
@@ -31,4 +56,5 @@ public class LoadImage extends Application {
         launch();
 
     }
+
 }
