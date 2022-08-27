4ea669edc800d27571482cc54f51ddddd5e482a1
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2022-08-27 17:35:24 -0500

    load music

---
+++ src/main/java/com/example/demo/SongPlayer.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class SongPlayer extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Song Player");
+        Group StartGroup = new Group();
+        Scene main = new Scene(StartGroup, 320 , 400);
+
+
+        System.out.println(StartGroup.getChildren());
+
+    }
+
+    public static void main(String[] args){
+        launch();
+    }
+}
